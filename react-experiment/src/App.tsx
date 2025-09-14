2f83ffabc85e25af7d6fb20ff2c72f414bed2334
Chribit <29504473+Chribit@users.noreply.github.com>
2025-09-14 18:25:36 +0200

    add typed props and basic message chat rendering

--- react-experiment/src/App.tsx
+++ react-experiment/src/App.tsx
@@ -1,35 +1,62 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Message } from "./Message";
+import type { MessageT, AuthorT } from "./Message";
 
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+function randomIndex (maximum : number)
+{
+    return Math.floor(Math.random() * maximum);
 }
 
-export default App
+function App ()
+{
+    const authors : AuthorT[] = [
+        { name: "Rick Sanchez", id: 0 },
+        { name: "Morty Smith", id: 1 },
+        { name: "Jerry Smith", id: 2 },
+        { name: "Summer Smith", id: 3 },
+        { name: "Beth Smith", id: 4 },
+        { name: "Tammy Gueterman", id: 5 },
+        { name: "Squanchy", id: 6 },
+        { name: "Gene Gilligan", id: 7 },
+        { name: "Andre Curtis", id: 8 },
+        { name: "Birdperson Phoenixperson", id: 9 }
+    ];
+    const authorCount : number = authors.length;
+
+    const messages : MessageT[] = [
+        { author: authors[randomIndex(authorCount)], content: "Hello!" },
+        { author: authors[randomIndex(authorCount)], content: "Hi!" },
+        { author: authors[randomIndex(authorCount)], content: "Omg hello!" },
+        { author: authors[randomIndex(authorCount)], content: "This is amazing!" },
+        { author: authors[randomIndex(authorCount)], content: "React is so fun, holy sh*t." },
+        { author: authors[randomIndex(authorCount)], content: "Look at us go!" },
+        { author: authors[randomIndex(authorCount)], content: "Look at all these messages coming in, wow." },
+        { author: authors[randomIndex(authorCount)], content: "This is so efficient." },
+        { author: authors[randomIndex(authorCount)], content: "Someone should hire the programmers behind this!" },
+        { author: authors[randomIndex(authorCount)], content: "Absolutely." },
+        { author: authors[randomIndex(authorCount)], content: "Agreed!" },
+        { author: authors[randomIndex(authorCount)], content: "Hello world!" },
+        { author: authors[randomIndex(authorCount)], content: "Is this a project initialisation or what?" },
+        { author: authors[randomIndex(authorCount)], content: "What world are you talking to?!" },
+        { author: authors[randomIndex(authorCount)], content: "Maybe they're just crazy..." },
+        { author: authors[randomIndex(authorCount)], content: "Happens to the best of us!" },
+        { author: authors[randomIndex(authorCount)], content: "I wonder how far this will all go." },
+        { author: authors[randomIndex(authorCount)], content: "This is indeed fascinating." },
+        { author: authors[randomIndex(authorCount)], content: "We seem to randomly change appearances..." },
+        { author: authors[randomIndex(authorCount)], content: "What the hell?!" }
+    ];
+
+    return(
+        <>
+            <h1>Intergalactic Chat</h1>
+            <ul className="messages">
+                {
+                    messages.map((message : MessageT) => (
+                        <Message message={message} />
+                    ))
+                }
+            </ul>
+        </>
+    );
+}
+
+export default App;
---
+++ react-experiment/src/Message.tsx
@@ -0,0 +1,34 @@
+import "./Message.css"
+
+export type AuthorT = {
+    name: string;
+    id: number;
+};
+
+export type MessageT = {
+    author: AuthorT;
+    content: string;
+};
+
+type MessageProps = {
+    message : MessageT
+}
+
+function randomKey ()
+{
+    return Math.floor(Math.random() * 999999999);
+}
+
+function Message ({ message } : MessageProps)
+{
+    return(
+        <li key={randomKey()} className="message">
+            <span className="messageAuthor">{message.author.name}</span>
+            <span className="messageContent">{message.content}</span>
+        </li>
+    );
+}
+
+export {
+    Message
+};
